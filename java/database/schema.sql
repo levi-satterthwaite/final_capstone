536b92497616864490f97bf40d3b7437eedb32dc
Levi Satterthwaite <levi.satt001@gmail.com>
2021-07-31 11:21:21 -0400

    establed database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -21,5 +21,56 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+DROP TABLE IF EXISTS recipe_ingredient;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS ingredient;
+
+CREATE TABLE recipe (
+        recipe_id serial primary key,
+        name varchar(255) not null,
+        category varchar(255) not null,
+        difficulty_level varchar(255) not null,
+        prep_time_min int not null,
+        cook_time_min int not null,
+        serving_size int not null,
+        instructions text not null,
+        date_created date not null
+);
+
+CREATE TABLE ingredient (
+        ingredient_id serial primary key,
+        name varchar(255) not null,
+        category varchar(255) not null
+);
+
+CREATE TABLE recipe_ingredient (
+        recipe_id int not null,
+        ingredient_id int not null,
+        quantity decimal(5,2) not null,
+        unit_measurement varchar(255) not null,
+        
+        CONSTRAINT pk_recipe_ingredient primary key(recipe_id, ingredient_id),
+        CONSTRAINT fk_recipe_ingredient_recipe_id foreign key(recipe_id) references recipe(recipe_id),
+        CONSTRAINT fk_recipe_ingredient_ingredient_id foreign key(ingredient_id) references ingredient(ingredient_id)
+);
+
+INSERT INTO ingredient (ingredient_id, name, category) VALUES (DEFAULT, 'Sourdough Bread', 'Bread');
+INSERT INTO ingredient (ingredient_id, name, category) VALUES (DEFAULT, 'Unsalted Butter', 'Dairy');
+INSERT INTO ingredient (ingredient_id, name, category) VALUES (DEFAULT, 'Mayonnaise', 'Condiment');
+INSERT INTO ingredient (ingredient_id, name, category) VALUES (DEFAULT, 'Cheddar Cheese', 'Dairy');
+
+INSERT INTO recipe (recipe_id, name, category, difficulty_level, prep_time_min, cook_time_min, serving_size, instructions, date_created) 
+VALUES (DEFAULT, 'Grilled Cheese', 'Comfort Food', 'Easy', 5, 7, 1, 'On a cutting board, butter each piece of bread with butter on one side. 
+Flip the bread over and spread each piece of bread with mayonnaise. 
+Place the cheese on the buttered side of one piece of bread. Top it with the second piece of bread, mayonnaise side out. 
+Heat a nonstick pan over medium low heat. 
+Place the sandwich on the pan, mayonnaise side down. 
+Cook for 3-4 minutes, until golden brown. 
+Using a spatula, flip the sandwich over and continue cooking until golden brown, about 2-3 minutes.', '2021-07-31');
+
+INSERT INTO recipe_ingredient (recipe_id, ingredient_id, quantity, unit_measurement) VALUES (1, 1, 2, 'Slices');
+INSERT INTO recipe_ingredient (recipe_id, ingredient_id, quantity, unit_measurement) VALUES (1, 2, 1.5, 'Tablespoons');
+INSERT INTO recipe_ingredient (recipe_id, ingredient_id, quantity, unit_measurement) VALUES (1, 3, 1.5, 'Tablespoons');
+INSERT INTO recipe_ingredient (recipe_id, ingredient_id, quantity, unit_measurement) VALUES (1, 4, 3, 'Slices');
 
 COMMIT TRANSACTION;
