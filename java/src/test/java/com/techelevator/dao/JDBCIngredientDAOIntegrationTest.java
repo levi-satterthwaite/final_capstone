79b94af5c6350aacd454d0d2203831ba091e1269
Samina Shaw <saminashaw3@gmail.com>
2021-08-02 18:30:06 -0400

    new ingredient test added

--- java/src/test/java/com/techelevator/dao/JDBCIngredientDAOIntegrationTest.java
+++ java/src/test/java/com/techelevator/dao/JDBCIngredientDAOIntegrationTest.java
@@ -2,12 +2,16 @@ package com.techelevator.dao;
 
 import com.techelevator.recipes.dao.IngredientDAO;
 import com.techelevator.recipes.dao.JdbcIngredientDAO;
+import com.techelevator.recipes.dao.JdbcRecipeDAO;
+import com.techelevator.recipes.dao.RecipeDAO;
 import com.techelevator.recipes.model.Ingredient;
+import com.techelevator.recipes.model.Recipe;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +19,14 @@ public class JDBCIngredientDAOIntegrationTest extends DAOIntegrationTest{
 
     private JdbcTemplate jdbcTemplate;
     private IngredientDAO ingredientDAO;
+    private RecipeDAO recipeDAO;
 
     @Before
     public void setup() {
         jdbcTemplate = new JdbcTemplate(getDataSource());
         ingredientDAO = new JdbcIngredientDAO(jdbcTemplate);
+        recipeDAO = new JdbcRecipeDAO(jdbcTemplate, ingredientDAO);
+
     }
 
     @Test
@@ -53,7 +60,29 @@ public class JDBCIngredientDAOIntegrationTest extends DAOIntegrationTest{
         Assert.assertTrue(testIngredientList.size() > 0);
 
     }
-    
+
+    @Test
+    public void retrieve_ingredients_by_recipeId() {
+        //making and adding a recipe to the database
+        Recipe recipeOne = getRecipe(-1L);
+        recipeDAO.addRecipe(recipeOne);
+        //making and adding an ingredient to the database
+        Ingredient ingredientOne = getIngredient(-1L);
+        ingredientOne.setUnitMeasurement("unitMeasurement");
+        ingredientOne.setQuantity(1.5);
+        ingredientDAO.addIngredient(ingredientOne); //adding ingredient to database
+        //making a list to hold our ingredient
+        List<Ingredient> ingredients = new ArrayList<Ingredient>();
+        ingredients.add(ingredientOne);
+        //will now put those ingredients into our recipe
+        recipeDAO.addIngredientsToRecipe(recipeOne, ingredients);
+
+        //get a recipe's ingredients (assert True will tell us if we added because > 0 will be true.
+        List<Ingredient> testIngredientList = ingredientDAO.getIngredientsByRecipeId(recipeOne.getRecipeId());
+        Assert.assertTrue(testIngredientList.size() > 0);
+    }
+
+
     private Ingredient getIngredient(Long ingredientId) {
         Ingredient ingredient = new Ingredient();
         ingredient.setIngredientId(ingredientId);
@@ -70,6 +99,21 @@ public class JDBCIngredientDAOIntegrationTest extends DAOIntegrationTest{
         ingredient.setCategory("testCategory");
         return ingredient;
     }
-
+    
+    private Recipe getRecipe(Long recipeId) {
+        Recipe recipe = new Recipe();
+        recipe.setRecipeId(recipeId);
+        recipe.setName("testName");
+        recipe.setCategory("testCategory");
+        recipe.setDifficultyLevel("testDifficulty");
+        recipe.setPrepTimeMin(5);
+        recipe.setCookTimeMin(6);
+        recipe.setServingSize(4);
+        recipe.setInstructions("testInstructions");
+        recipe.setDateCreated(LocalDate.of(2021, 8, 2));
+        recipe.setImageFileName("testImage.jpg");
+        recipe.setIngredientList(new ArrayList<>());
+        return recipe;
+    }
 
 }
