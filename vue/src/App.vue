7b4899aeda3e7d67c187c2dd52de93a5564d3560
Bo Hong <bhh920@gmail.com>
2021-08-03 13:00:42 -0400

    added signin link

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,8 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{name: 'recipes'}">Recipes</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'recipes' }">Recipes</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
+      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"
+        >Login</router-link
+      >
     </div>
     <router-view />
   </div>
