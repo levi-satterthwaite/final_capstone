5aa8a29ee808188633fab2918bb736ff9ece28e8
Bo Hong <bhh920@gmail.com>
2021-08-02 23:23:06 -0400

    created a new generic search autocomplete component

---
+++ vue/src/components/SearchAutocomplete.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="autocomplete">
+    <input
+      type="text"
+      v-model="searchTerm"
+      v-on:input.prevent="onChange"
+      v-bind:id="id"
+      v-on:keydown.down="onArrowDown"
+      v-on:keydown.up="onArrowUp"
+      v-on:keydown.enter.prevent="onEnter"
+    />
+    <ul class="autocomplete-results" v-show="isOpen">
+      <li class="loading" v-if="isLoading">Searching...</li>
+      <li
+        class="autocomplete-result"
+        v-else
+        v-for="(result, i) in results"
+        v-bind:key="i"
+        v-on:click.prevent="setResult(result)"
+        v-bind:class="{ 'is-active': i === arrowCounter }"
+      >
+        {{ result.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchAutocomplete",
+  props: {
+    id: null,
+    getData: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      searchTerm: "",
+      results: [],
+      // toggle for deciding whether we display results on the screen or not
+      isOpen: false,
+      arrowCounter: -1,
+      isLoading: false,
+      isLoaded: false,
+      error: null
+    };
+  },
+  methods: {
+    filterResults() {
+      // iterating over every item and checking if item has the search term
+      // in order to filter the results
+      this.results = this.items.filter((item) => {
+        return item.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+      });
+    },
+    async onChange() {
+      this.$emit('change', this.searchTerm);
+      // if we are already loading data from the getData function
+      // just return we still need to wait for the data
+      if (this.isLoading) {
+        return; // stop the function
+      }
+
+      // if we have not loaded data then we will load the data
+      if (!this.isLoaded) {
+        // since we are about to start loading we need to set isLoading to true
+        this.isLoading = true;
+        // we need to get the data. getData could return promise. Since it could return
+        // a promise, we are using async / await. This will help us keep the code
+        // cleaner and simpler.
+        try {
+          this.items = await this.getData();
+        } catch (error) {
+          // if we get an error we will store it into the component so 
+          // we can display the error message to the user
+          this.error = error;
+        }
+        // now that we are done loading we need to set the isLoading to false and
+        // set isLoaded true
+        this.isLoading = false;
+        this.isLoaded = true;
+      }
+
+      // since we are not loading data and data has been loaded we can filter what
+      // the user sees based on what the user has typed
+      this.filterResults();
+
+      // we need to make sure the the results can be displayed so we are 
+      // setting isOpen true
+      this.isOpen = true;
+    },
+    setResult(result) {
+      this.searchTerm = '';
+      this.selectedResult = result;
+      this.isOpen = false;
+      this.$emit('result', result);
+    },
+    handleClickOutside(event) {
+      // $el is the html element associated with this component
+      // if the html element doesn't contain the event target, close the results
+      if (!this.$el.contains(event.target)) {
+        this.arrowCounter = -1;
+        this.isOpen = false;
+      }
+    },
+    onArrowDown() {
+      if (this.arrowCounter < this.results.length) {
+        this.arrowCounter = this.arrowCounter + 1;
+      }
+    },
+    onArrowUp() {
+      if (this.arrowCounter > 0) {
+        this.arrowCounter = this.arrowCounter - 1;
+      }
+    },
+    onEnter() {
+      const result = this.results[this.arrowCounter];
+      this.arrowCounter = -1;
+      this.setResult(result);
+    },
+  },
+  mounted() {
+    // document is a global variable that represents all the html element
+    document.addEventListener("click", this.handleClickOutside);
+  },
+  destroyed() {
+    document.removeEventListener("click", this.handleClickOutside);
+  },
+};
+</script>
+
+<style>
+.autocomplete {
+  position: relative;
+}
+
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  height: 120px;
+  min-height: 1em;
+  max-height: 6em;
+  overflow: auto;
+}
+
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+}
+
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
+}
+
+.autocomplete-result.is-active,
+.autocomplete-result:hover {
+  background-color: #4aae9b;
+  color: white;
+}
+</style>
