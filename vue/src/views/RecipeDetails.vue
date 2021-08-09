fef0c99773086d1c11d9f22ef0efb052980206b6
Samina Shaw <saminashaw3@gmail.com>
2021-08-09 19:04:35 -0400

    recipe details card

--- vue/src/views/RecipeDetails.vue
+++ vue/src/views/RecipeDetails.vue
@@ -1,14 +1,26 @@
 <template>
  <div class="recipe-details">
- <p class="description"></p>
-
+ <p class="description">{{ description }}</p>
  <recipe-card v-bind:recipe = "recipe"/> <!-- this is our template for recipe binding to object -->  
+ 
+ <div id="recipeDetail">
+     <h1>{{recipe.name}}</h1>
+      <img :src="imageFileName" />
+      <p><a v-bind:href="imageFileName">{{recipe.name}}</a></p>
+      <p v-for="ingredient in recipe.ingredientList" 
+        v-bind:key="ingredient.id">
+        {{ingredient.name}}
+       </p>
+      <p>{{ recipe.instructions }}</p>
+
+ </div>
  </div>
 </template>
 
 <script>
 import RecipeCard from '../components/RecipeCard.vue';
 export default {
+    name: 'recipe-detail',
     data () {
         return{
             recipe: undefined,
@@ -27,6 +39,8 @@ export default {
     },
     components: {
         RecipeCard,
+        
+
     }
     
 }
