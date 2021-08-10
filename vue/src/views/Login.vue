89c4816dffbbef961329d5bcf0a6e613c5233a39
Bo Hong <bhh920@gmail.com>
2021-08-09 23:04:58 -0400

    added styling and tag classes to format login page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,39 +1,49 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
-    </form>
+  <div class="page">
+    <div id="login" class="form text-center card">
+      <form class="form-signin" @submit.prevent="login">
+        <h2 class="h3 mb-3 font-weight-normal">Please Sign In</h2>
+        <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+          Invalid username and password!
+        </div>
+        <div
+          class="alert alert-success"
+          role="alert"
+          v-if="this.$route.query.registration"
+        >
+          Thank you for registering, please sign in.
+        </div>
+        <div class="field">
+          <label for="username" class="sr-only">Username</label>
+          <input
+            type="text"
+            id="username"
+            class="form-control"
+            placeholder="Username"
+            v-model="user.username"
+            required
+            autofocus
+          />
+        </div>
+        <div class="field">
+          <label for="password" class="sr-only">Password</label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+        </div>
+        <div class="register">
+           <router-link :to="{ name: 'register' }">Need an account?</router-link>
+        </div>
+        <div class="form-controls align-right">
+          <button class="btn" type="submit">Sign in</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -47,30 +57,59 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style>
+div#login {
+  margin-top: 50px;
+}
+div#login h2 {
+  margin-top: 20px;
+  margin-bottom: 40px;
+  color: #4b3f72;
+  font-size: 35px;
+}
+div#login div.field label {
+  font-size: 20px;
+  color: #4b3f72;
+}
+div.field input#username {
+  font-size: 18px;
+}
+div.field input#password {
+  font-size: 18px;
+}
+div.register {
+  margin: 10px;
+  font-size: 18px;
+}
+div#login div.form-controls button.btn {
+  font-size: 18px;
+}
+</style>
