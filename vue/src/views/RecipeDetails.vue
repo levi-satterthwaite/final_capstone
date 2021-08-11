4a424bf41336f43869dd89f339861714a26bc9ed
Bo Hong <bhh920@gmail.com>
2021-08-10 21:23:06 -0400

    rendered image to recipe details page; reogranized html elements; added a compute for imageFIleName; clean up

--- vue/src/views/RecipeDetails.vue
+++ vue/src/views/RecipeDetails.vue
@@ -1,59 +1,78 @@
 <template>
- <div class="recipe-details">
- <p class="description">{{ description }}</p>
- <recipe-card v-bind:recipe = "recipe"/> <!-- this is our template for recipe binding to object -->  
- 
- <div id="recipeDetail">
-     <h1>{{recipe.name}}</h1>
-      <img :src="imageFileName" />
-      <p><a v-bind:href="imageFileName">{{recipe.name}}</a></p>
-      <p v-for="ingredient in recipe.ingredientList" 
-        v-bind:key="ingredient.id">
-        {{ingredient.name}}
-       </p>
+  <div class="page recipe-details">
+    <div class="error" v-if="error">{{ error.message }}</div>
+    <div class="recipe-details-container card">
+      <img v-if="imageFileName" v-bind:src="imageFileName" />
+      <h1>{{ recipe.name }}</h1>
+      <p>
+        <a v-bind:href="imageFileName">{{ recipe.name }}</a>
+      </p>
+      <p v-for="ingredient in recipe.ingredientList" v-bind:key="ingredient.id">
+        {{ ingredient.name }}
+      </p>
       <p>{{ recipe.instructions }}</p>
-
- </div>
- </div>
+    </div>
+    <div class="action-bar" v-if="recipe">
+      <router-link
+        v-bind:to="{ name: 'updateRecipe', params: { id: recipe.recipeId } }"
+        tag="button"
+        class="btn"
+        >Edit Recipe</router-link
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-import RecipeCard from '../components/RecipeCard.vue';
-export default {
-    name: 'recipe-detail',
-    data () {
-        return{
-            recipe: undefined,
-        }
-    },
-    created () {
-        console.log(this.$route.params.id)
-        const findRecipeById = this.$store.state.recipes.find ((r) => {
-            if(r.recipeId == this.$route.params.id) {
-                return true
-            } else {
-                return false
-            }
-        });
-        this.recipe = findRecipeById
-    },
-    components: {
-        RecipeCard,
-        
+import mealPlannerService from "@/services/MealPlannerService";
 
+export default {
+  name: "recipe-detail",
+  data() {
+    return {
+      recipe: null,
+      error: null,
+    };
+  },
+  computed: {
+    imageFileName() {
+      if (!this.recipe) {
+        return null;
+      }
+      return "/files/" + this.recipe.imageFileName;
     }
-    
-}
-    
+  },
+  async created() {
+    const recipeId = this.$route.params.id;
+    this.recipe = this.$store.state.recipes.find((recipe) => {
+      return recipe.recipeId == recipeId;
+    });
+    if (!this.recipe) {
+      try {
+        const response = await mealPlannerService.getRecipeById(recipeId);
+        this.recipe = response.data;
+      } catch (e) {
+        this.error = mealPlannerService.getError(e);
+      }
+    }
+  },
+};
 </script>
 
 <style>
-div.description {
-    color: #4B3F72;
+div.recipe-details {
+  display: flex;
+  flex-grow: 1;
 }
-#recipeDetail {
-    text-align: left;
-    font-size: 15pt;
-    color: #4B3F72;
+div.recipe-details-container {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  width: 70%;
+  margin-top: 25px;
+}
+div.recipe-details-container img {
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
 }
 </style>
