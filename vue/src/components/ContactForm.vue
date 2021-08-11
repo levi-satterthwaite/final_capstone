4ad6df2a87c6a6b3f0e30fec2995074b5d97def3
Samina Shaw <saminashaw3@gmail.com>
2021-08-10 22:23:52 -0400

    email JS integration

---
+++ vue/src/components/ContactForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="contact-form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+</template>
+
+<script>
+import emailjs from '@emailjs-com';
+
+export default {
+  methods: {
+   sendEmail: (e) => {
+      emailjs.sendForm('service_2ax4geo', 'ejs-test-mail-service', e.target, 'user_mGcL0LdrwUOOtseA6Bbfe')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+</style>
